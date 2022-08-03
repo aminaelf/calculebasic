2bb0907c83c63377dbaee26bb726571548d5d6fd
aminaelf <aminaelfarissi37@gmail.com>
2022-08-03 12:36:30 +0200

    calculeswitch.c
    calculate

--- calculeswitch.c
+++ calculeswitch.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
-int main(){
-    int num1,num2;
-    char operateur , q;
-
+#include <stdlib.h>
+int main()
+{
+    int num1, num2;
+    char operateur;
     printf("Entrer le nombre1 :");
-    scanf("%d" ,&num1);
+    scanf("%d", &num1);
     printf("Entrer l'operateur :");
-    scanf("%s" ,&operateur);
-    printf("Entrer le nombre2 :");
-    scanf("%d" ,&num2);
-
-     switch (operateur)
+    scanf("%s", &operateur);
+    if (operateur == 'q')
     {
-    case'+': printf("le resulta est %d : ",num1+num2);
-        break;
 
-    case'-': printf("le resulta est %d : ",num1-num2);
-        break;
-    case '*': printf("le resulta est %d : ",num1*num2);
-        break;
-    case '/': if(num2!=0) 
-    printf("le resultat est %d : ",num1/num2);
-        else
-        printf("impossible  de diviser sur 0");
-        break;
-    default : printf("cet operateur n'exicte pas ");
-        break;
-case'q' :
-printf("programme terminer");
-break;
+        printf("programme arreter!!");
+        exit(0);
+    }
+    else
+    {
+        printf("Entrer le nombre2 :");
+        scanf("%d", &num2);
+
+        switch (operateur)
+        {
+        case '+':
+            printf("le resulta est %d : ", num1 + num2);
+            break;
+
+        case '-':
+            printf("le resulta est %d : ", num1 - num2);
+            break;
+        case '*':
+            printf("le resulta est %d : ", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("le resultat est %d : ", num1 / num2);
+            else
+                printf("impossible  de diviser sur 0");
+            break;
+        default:
+            printf("cet operateur n'exicte pas ");
+            break;
+        }
     }
 }
